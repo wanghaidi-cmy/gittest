2c822a44687d3336cb59f13d9821918c56177701
wanghaidi <2792026415@qq.com>
2022-01-24 16:58:48 +0800

    初始化gittest项目 修改配置文件
    Signed-off-by: wanghaidi <2792026415@qq.com>

--- src/test/java/com/whd/pojo/UserTest.java
+++ src/test/java/com/whd/pojo/UserTest.java
@@ -5,4 +5,10 @@ package com.whd.pojo;
  * @create 2022-01-24 16:43
  */
 public class UserTest {
+<<<<<<< HEAD
+=======
+    public static void main(String[] args) {
+
+    }
+>>>>>>> 405b852 (初始化gittest项目)
 }
